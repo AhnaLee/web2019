005e2fe2114d4666abdb64002e870049845b9322
Ahna Lee <52556893+AhnaLee@users.noreply.github.com>
2019-12-23 14:56:28 +0900

    20191223
    update

--- j-Query/20191219_jquery/js/script.js
+++ j-Query/20191219_jquery/js/script.js
@@ -160,4 +160,96 @@ $(document).ready(function(){
 	 	$("html, body").animate({scrollTop:$("body").offset().top},2000);
 	 });
 
+	 //템플릿 리터럴 = 자바스크립트 코딩상 한줄을 저장할수는 있지만 여러줄(엔터값 포함)을 저장하고자 할 때 (`html,문서`)사용 
+	 var $temp = `
+	 <li>
+	 	<a href="#">경영소개</a>
+	 </li>
+	  <li>
+	 	<a href="#">프로젝트</a>
+	 </li>`;
+
+	 //before() 메서드 : 선택한 요소의 이전 형제로 새로운 요소(들)을 추가
+	 $(".before_after_add li").eq(1).before($temp);
+	 // $(".before_after_add li").eq(1).before("<li><a href='#'>경영소개</a></li>"); //한줄용
+
+	 //after () 메서드 : 선택한 요소의 다음 형제로 새로운 요소(들)을 추가
+	$(".before_after_add li").last(1).after("<li><a href='#'>공지사항</a></li>");
+
+	$("<h5>이벤트 기간 : 2019.12.23~2019.12.31</h5>").insertBefore(".insert_before_after h4");
+	$("<p>엄마는 외계인</p>").insertAfter(".insert_before_after p:eq(2)");
+
+	//append () 메서드
+	$(".append ul").append("<li><a href=''>Portfolio</a></li>")
+	// prepend() 메서드
+	$(".prepend").prepend("<p>영국</p>");
+	/*
+		#1. 배열값으로 메뉴에 대한 항목을 저장
+		#2. for 문을 이용하여 구조를 .add_menu 의 자식으로 반복하여 넣겠다
+		#3. each 문을 활용하여 배열 값을 각각의 분기점 항목의 내부에 넣겠다.
+
+		*2차배열을 활용한 항목 넣기 (메뉴이름,링크주소)*
+	*/
+	var $menu_arr = [
+	["About", "about.html"], // 0
+	["Dishes", "dishes.html"], //1
+	["Event", "event.html"], //2
+	["Notice", "notice.html"] //3
+	];
+	for (i=0; i<$menu_arr.length; i++){
+		$(".add_menu").append("<li><a href=''>"+i+"</a></li>");
+	}; //for문 (반복)으로 구조(li, a)를 만들고나서 밑에로 내려가 메뉴를 구성
+
+	$(".add_menu li").each(function(index){ //each (각각)적용해라
+		console.log(index);
+		$(this).find("a").text($menu_arr[index][0]); //this = li ,find("태그")
+		$(this).find("a").attr("href", $menu_arr[index][1]); //2번째 배열 attr(링크)
+	});
+
+	// //setInterval
+	// setInterval(function(){
+	// 	var $f_img = "<li><img src='img/tree-1.jpg' alt=''></li>"; // 실제 문서를 데이터 형태로 저장
+	// 	console.log($f_img);
+	// 	var $f_img = $(".slide li").eq(0); //첫번째 <li>이하를 저장 
+	// 	//선택자를 객체 저장 (내부의 항목 뿐만 아니라 내부의 DOM 요소들을 포함한 모든 노드값을 모두 가져온다.)
+	// 	console.log($f_img);
+	// 	var $f_img = $(".slide li").eq(0).html;
+	// 	console.log($f_img);
+	// 	$(".slide").append($f_img); //저장된 첫번째 <li> 이하를 부모영역인 .slide의 맨 마지막 자식으로 넣어주겠다.
+	//  }, 1000);
+
+	var $c_img = $(".card_img"); //객체로 접근하여 저장 -객체 데이터.
+	console.log($c_img);
+
+	$(".in_card p").after($c_img);
+
+	var $color_clone = $(".clone p:eq(0)").clone(true);
+	console.log($color_clone);
+	$(".clone").append($color_clone);
+
+	//clone(true) : 선택한 항목의 하위 요소를 모두 복제하겠다는 의미를 갖고있음.
+
+	$(".empty p").empty(); //내부내용만 사라지고 본인(p)는 남아있음
+	$(".remove").remove();
+
+	$(".replaceWith p").replaceWith("<p>오렌지</p>")
+
+	$(".unwrap p").unwrap(); //선택자의 부모 영역 제거
+//	$(".unwrap p").wrap("<div />"); //p 태그를 div로 하나하나 감싸기
+    $(".unwrap p").wrapAll("<div />"); // 모든 p 태그를 하나의 div로 감싸기
+    $(".unwrap p").wrapInner("<a />"); //p 태그 내부를 a 태그로 감싸기
+
+    $("#myList li").not(":first").css("background","blue");
+    // $("#myList li").not("#myList li:eq(0)").css("background","blue");
+    $("#myList li:eq(2)").css("background","green");
+    	
+
+
+
+
+
+
+
+
+
 });
